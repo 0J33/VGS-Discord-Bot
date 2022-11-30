dd5db8e28aca4c5eb5fb8d2741bb9223a0e6e474
Ojee <90639992+0J33@users.noreply.github.com>
2022-11-30 23:15:04 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,31 @@
+# create new command
 @client.tree.command(name="test", description="test description")
-@app_commands.describe(testin = "test input")
-async def test(interaction: discord.Interaction, testin: str):
+# if command has input
+@app_commands.describe(test_input = "test input")
+# the command is like a method that takes input and it's type
+async def test(interaction: discord.Interaction, test_input: str):
 
+    # defer the response while bot processes the command
     await interaction.response.defer()
     await asyncio.sleep(1)
 
+    # if time is needed
     datetime = await get_time()
     datetime = datetime.replace(" ", "")
     datetime = datetime.replace(":", "")
 
     try:
+        # open text file if necessary
         f = open(r"path\to\file.txt", "r")
+        # read from the file
         msg = f.read()
+        # set image ot txt file as attachment in the embed
         file=discord.File(r"path\to\file.png", filename="image.png")
+        # set the embed's title and description and the message string
         embed = discord.Embed(title="", description=f"{interaction.user.mention}\n" + msg,colour=discord.Color.from_rgb(25, 25, 26))
+        # set the embed's image from attachment
         embed.set_image(url="attachment://image.png")
+        # send the response
         await interaction.followup.send(file=file,embed=embed)
     except Exception as exc:
         print(exc)
